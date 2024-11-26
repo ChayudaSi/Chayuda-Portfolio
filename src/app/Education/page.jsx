14e55f2796bd7bc766e1fb8ee2862fcff9eba70a
ChayudaSi <n.sirithan16@gmail.com>
2024-11-26 17:00:25 +0700

    Modify responsitive

--- src/app/Education/page.jsx
+++ src/app/Education/page.jsx
@@ -74,7 +74,7 @@ const Pillar= () => {
 
 const EventCard = ({year, title,location,description}) => {
   return (
-    <div className = "rounded-lg bg-gray-800 transition duration-300 ease-in-out transform hover:scale-105 hover:shadow-2xl flex flex-col gap-y-2 shadow-md rounded-xml-xl p-4">
+    <div className = "rounded-lg bg-gray-800 transition duration-300 ease-in-out transform hover:scale-105 hover:shadow-2xl flex flex-col gap-y-2 shadow-md rounded-xml-xl p-4 flex-wrap">
         <div className = "text-white flex justify-end">
           <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 2 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
             <path stroke-linecap="round" stroke-linejoin="round" d="M6.75 3v2.25M17.25 3v2.25M3 18.75V7.5a2.25 2.25 0 0 1 2.25-2.25h13.5A2.25 2.25 0 0 1 21 7.5v11.25m-18 0A2.25 2.25 0 0 0 5.25 21h13.5A2.25 2.25 0 0 0 21 18.75m-18 0v-7.5A2.25 2.25 0 0 1 5.25 9h13.5A2.25 2.25 0 0 1 21 11.25v7.5" />
