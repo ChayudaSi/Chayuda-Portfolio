f6b55bd9675b1886158798592d96490ba74a5de6
ChayudaSi <n.sirithan16@gmail.com>
2024-11-26 17:29:00 +0700

    Modify responsitive

--- src/app/Education/page.jsx
+++ src/app/Education/page.jsx
@@ -87,9 +87,9 @@ const EventCard = ({year, title,location,description}) => {
             <path stroke-linecap="round" stroke-linejoin="round" d="M15 10.5a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
             <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 10.5c0 7.142-7.5 11.25-7.5 11.25S4.5 17.642 4.5 10.5a7.5 7.5 0 1 1 15 0Z" />
             </svg>  
-            <h2 className = "text-l text-white-800 text-white mx-2">{location}</h2>
+            <h2 className = "text-l -bold text-white text-white mx-2">{location}</h2>
         </div>
-        <p className = "text-sm text-gray-400 ">{description}</p>
+        <p className = "mx-3 mt-2 text-gray-400 ">{description}</p>
     </div>
   )
 }
--- src/app/Experience/page.jsx
+++ src/app/Experience/page.jsx
@@ -82,7 +82,7 @@ const EventCard = ({role, location,date,description}) => {
           <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 2 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
             <path stroke-linecap="round" stroke-linejoin="round" d="M6.75 3v2.25M17.25 3v2.25M3 18.75V7.5a2.25 2.25 0 0 1 2.25-2.25h13.5A2.25 2.25 0 0 1 21 7.5v11.25m-18 0A2.25 2.25 0 0 0 5.25 21h13.5A2.25 2.25 0 0 0 21 18.75m-18 0v-7.5A2.25 2.25 0 0 1 5.25 9h13.5A2.25 2.25 0 0 1 21 11.25v7.5" />
           </svg>
-          <h3 className= "mx-2 font-bold">{date}</h3>
+          <h3 className= "text-right text-white font-bold mx-2">{date}</h3>
         </div>
         <h3 className = "text-xl text-transparent bg-clip-text bg-gradient-to-r text-bold from-green-400 to-blue-500 font-bold text-lg border-b">{role}</h3>
         <div className = "flex text-white">
