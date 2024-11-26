843e9a44775ae30cdb2eb75550058aef9dd001ac
ChayudaSi <n.sirithan16@gmail.com>
2024-11-26 17:14:14 +0700

    Modify responsitive

--- src/app/Education/page.jsx
+++ src/app/Education/page.jsx
@@ -21,8 +21,8 @@ const education = [
 
 const Timeline = ({events}) => {
   return (
-    <div className = "flex flex-col flex-wrap gap-y-3 w-full my-4"  id="education">
-      <div className = "mt-20 text-white font-extrabold text-4xl flex justify-center">
+    <div className = "flex flex-col gap-y-3 w-full my-4 items-center justify-center"  id="education">
+      <div className = "flex text-white items-center">
         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" class="size-10">
           <path stroke-linecap="round" stroke-linejoin="round" d="M4.26 10.147a60.438 60.438 0 0 0-.491 6.347A48.62 48.62 0 0 1 12 20.904a48.62 48.62 0 0 1 8.232-4.41 60.46 60.46 0 0 0-.491-6.347m-15.482 0a50.636 50.636 0 0 0-2.658-.813A59.906 59.906 0 0 1 12 3.493a59.903 59.903 0 0 1 10.399 5.84c-.896.248-1.783.52-2.658.814m-15.482 0A50.717 50.717 0 0 1 12 13.489a50.702 50.702 0 0 1 7.74-3.342M6.75 15a.75.75 0 1 0 0-1.5.75.75 0 0 0 0 1.5Zm0 0v-3.675A55.378 55.378 0 0 1 12 8.443m-7.007 11.55A5.981 5.981 0 0 0 6.75 15.75v-1.5" />
         </svg>
@@ -34,7 +34,7 @@ const Timeline = ({events}) => {
           {/* Circle */}
           <Circle />
           {/* Timeline content */}
-          <div className = "grid grid-cols-1 md:grid-cols-[1fr_auto_1fr] gap-x-4 items-center mx-auto">
+          <div className = "grid grid-cols-[1fr_auto_1fr] gap-x-4 p-4">
             {event.direction === 'left'?(
               <EventCard year={event.year} title = {event.title} location={event.location} description={event.description} className="text-center md:text-left"/>
             ):(
--- src/components/AboutActions.jsx
+++ src/components/AboutActions.jsx
@@ -41,5 +41,5 @@ function AboutActions() {
     </div>
   );
 }
-
 export default AboutActions;
+
